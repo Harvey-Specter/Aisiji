1889a73a789397a3c3b4e13e76ac4be6d48fd044
lihong <2258104212@qq.com>
2023-02-12 21:49:55 +0800

    tms

---
+++ tms.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Ams
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *hermes.com*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jianshu.com
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.6.3.min.js
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+(function() {
+    'use strict';
+
+   console.log('Ams_hello')
+   // alert('Ams_hello')
+})();
